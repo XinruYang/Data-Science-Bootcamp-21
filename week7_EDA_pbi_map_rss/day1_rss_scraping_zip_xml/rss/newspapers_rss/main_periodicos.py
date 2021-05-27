aa958fb86dd297562fffdc602a3e19fbfeb67aad
XinruYang <xyang02@ucm.es>
2021-05-27 09:24:33 +0200

    27 de mayo

---
+++ week7_EDA_pbi_map_rss/day1_rss_scraping_zip_xml/rss/newspapers_rss/main_periodicos.py
@@ -0,0 +1,47 @@
+from urllib.request import urlopen
+from xml.etree.ElementTree import parse
+import pandas as pd
+import json as js
+
+company = 'gobierno'
+#company2 = "el"
+
+newspapers = {
+    'Expansion': 'https://e00-expansion.uecdn.es/rss/empresas.xml',
+    'El economista': 'https://www.eleconomista.es/rss/rss-empresas.php',
+    'Cinco dias': 'https://cincodias.elpais.com/seccion/rss/companias/',
+    'El confidencial': 'https://rss.elconfidencial.com/empresas/'
+}
+
+tot_data = {
+    'Periodico': [],
+    'Empresa': [],
+    'Noticia': [],
+    'Fecha noticia': [],
+    'Link Noticia': []
+}
+
+for i in newspapers:
+    url_str = newspapers[i]
+    var_url = urlopen(url_str)
+    xmldoc = parse(var_url)
+
+    for item in xmldoc.iterfind('channel/item'):
+
+        #Dos condiciones:
+        # if company in item.findtext('title').lower() and company2 in item.findtext('title').lower():
+
+        if company in item.findtext('title').lower():
+            tot_data['Periodico'].append(i)
+            tot_data['Empresa'].append(company)
+            tot_data['Noticia'].append(item.findtext('title'))
+            tot_data['Fecha noticia'].append(item.findtext('pubDate'))
+            tot_data['Link Noticia'].append(item.findtext('link'))
+
+company_df = pd.DataFrame(tot_data)
+print("company_df.shape:", company_df.shape)
+company_df.to_excel('./company_data.xlsx')
+company_df.to_csv('./company_data.csv')
+company_df.to_json("company_data.json")
+print("\n FINISH \n")
+company_df
